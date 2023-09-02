310a6f63443e574f094367435f2e1869fe28c113
Duy Bao <bao1322002@gmail.com>
2023-09-02 16:14:24 +0700

    feat: navbar menu

--- src/app/layouts/navigation/layout.tsx
+++ src/app/layouts/navigation/layout.tsx
@@ -1,17 +1,42 @@
 'use client';
 
 import SearchInput from '@/app/components/searchInput/component';
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from '@nextui-org/react';
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Button,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
+} from '@nextui-org/react';
 import Image from 'next/image';
+import { useState } from 'react';
 export default function Navigation() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const menuItems = ['Trang chủ', 'Top Anime', 'Thể loại', 'Mùa'];
+
   return (
     // Để shouldHideOnScroll hoạt đồng thì cần thêm fixed cái navbar lại
     //   Nếu navbar không có className='fixed' shouldHideOnScroll thì lúc để relative sẽ chiếm diện tích
     // và navbar không thể tự làm mờ trên nền ảnh được, còn có thì thêm thoải mái
-    <Navbar className='fixed' shouldHideOnScroll>
+    <Navbar
+      className='fixed'
+      shouldHideOnScroll
+      isBordered
+      isMenuOpen={isMenuOpen}
+      onMenuOpenChange={setIsMenuOpen}
+    >
       <NavbarBrand>
         <p className='font-bold text-inherit'>AnimeWorld</p>
       </NavbarBrand>
+
+      <NavbarContent className='sm:hidden' justify='start'>
+        <NavbarMenuToggle aria-label={isMenuOpen ? 'Close menu' : 'Open menu'} />
+      </NavbarContent>
       <NavbarContent className='hidden sm:flex gap-4 space-x-1' justify='center'>
         <NavbarItem>
           <Link color='foreground' href='#'>
@@ -42,6 +67,22 @@ export default function Navigation() {
           <Button as={Link}>Đăng nhập</Button>
         </NavbarItem>
       </NavbarContent>
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              className='w-full'
+              color={
+                index === 2 ? 'warning' : index === menuItems.length - 1 ? 'danger' : 'foreground'
+              }
+              href='#'
+              size='lg'
+            >
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
     </Navbar>
   );
 }
